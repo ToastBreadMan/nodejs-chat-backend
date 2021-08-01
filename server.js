056230771190fe1292f7300f93f5257f486bd8d4
ToastBreadMan <xghostx2709@gmail.com>
2021-08-01 22:22:24 +0200

    crud features working fine have to add sending function

--- server.js
+++ server.js
@@ -23,36 +23,51 @@ app.use(cookieParser())
 
 io.on("connection", (socket) => {
     let user;
+    // SOLUTION IS HERE USE GET AND RESOLVE POMISE THE SET NEW DOC WITH db.put()
     socket.on('message', (data) => {
         io.emit('message', data)
         // find all here satus is offline and send to each of those email with nodemailer
         db.find({
             selector: {status: false},
-            fields: ['_id', 'email']
+            fields: ['_id', 'email', 'status']
         }).then((data) => {
             if (data.docs.length){
                 for (let i = 0; i <=data.docs.length-1;i++) {
-                    
+                    console.log(data.docs[i].email)
                 }
             }
         })
     })
+    // conn connlost loop finished just need some testing
     socket.on('join', (data) => {
-        console.log('user joined')
          db.find({
              selector: {email: data.email},
-             fields: ['_id']
+             fields: ['_id', 'email', 'status', '_rev']
          }).then((data) => {
             if (data.docs.length) {
              user = data.docs[0]   
-             db.get(user._id).then((data) => {
-                 data.status = true
-             }) 
+             db.get(user._id).then(function(doc) {
+                doc.status = true
+                return db.put(doc)
+            })
             }
          })
     })
     socket.on('disconnect', () => {
-        user.status = false
+        db.get(user._id).then(function(doc) {
+            doc.status = false
+            return db.put(doc)
+        })
+        db.find({
+            selector: {status: false},
+            fields: ['_id', 'email', 'status']
+        }).then((data) => {
+            if (data.docs.length){
+                for (let i = 0; i <=data.docs.length-1;i++) {
+                    console.log(data.docs[i].email)
+                }
+            }
+        })
     })
 })
 
